d85fa7da30fb49bfe5ce0073306ad61c99e4cf28
Knar <adbyerly@comcast.net>
2020-04-08 12:14:06 -0500

    Created some simple functions to handle moving motors with boundaries

--- Mechacockatiel.ino
+++ Mechacockatiel.ino
@@ -1,8 +1,8 @@
 #include <Herkulex.h>
 
-int motor0=0x00;
-int motor1=0x01;
-int motor2=0x02;
+int hip = 0x00;
+int face = 0x01;
+int head = 0x02;
 
 void setup()
 {
@@ -10,24 +10,43 @@ void setup()
   Serial.begin(115200);    // Open serial communications
   Serial.println("Begin");
   Herkulex.begin(115200,10,11); //open serial with rx=10 and tx=11
-  Herkulex.reboot(motor0); //reboot first motor
+  Herkulex.reboot(hip); //reboot first motor
   delay(500);
   Herkulex.initialize(); //initialize motors
   delay(2000);
 }
 
 void loop(){
-  float pot0 = (float)analogRead(A0);
-  float pot1 = (float)analogRead(A1);
-  float pot2 = (float)analogRead(A2);
-  
-  float rotation0 = (pot0 / 3.2) - 160;
-  float rotation1 = (pot1 / 3.2) - 160;
-  float rotation2 = (pot2 / 3.2) - 160;
+  float hipRotation = ((float)analogRead(A0) / 3.2) - 160;
+  float faceRotation = ((float)analogRead(A1) / 3.2) - 160;
+  float headRotation = ((float)analogRead(A2) / 3.2) - 160;
 
-  Herkulex.moveOneAngle(motor0, (float)rotation0, 0, LED_BLUE);
-  Herkulex.moveOneAngle(motor1, (float)rotation1, 0, LED_BLUE);
-  Herkulex.moveOneAngle(motor2, (float)rotation2, 0, LED_BLUE);
-  
+  rotateHipMotor(hipRotation, 0);
+  rotateFaceMotor(faceRotation, 0);
+  rotateHeadMotor(headRotation, 0);
   delay(33);
+}
+
+void rotateHipMotor(float position, int time) {
+  rotateMotor(hip, position, time, 100, 10);
+}
+
+
+void rotateFaceMotor(float position, int time) {
+  rotateMotor(face, position, time, 150, -150);
+}
+
+void rotateHeadMotor(float position, int time) {
+  rotateMotor(head, position, time, 150, -150);
+}
+
+void rotateMotor(int motorId, float position, int time, float upperBound, float lowerBound) {
+  if (position > upperBound) {
+    position = upperBound;
+  }
+  if (position < lowerBound) {
+    position = lowerBound;
+  }
+  
+  Herkulex.moveOneAngle(motorId, position, time, LED_BLUE);
 }
