488dda7a304c0dfddc0ab59eb7a91efb0b665269
Knar <knarxr@protonmail.com>
2020-04-08 18:28:20 -0500

    Lots of refactors

--- Mechacockatiel.ino
+++ Mechacockatiel.ino
@@ -1,45 +1,53 @@
 #include <Herkulex.h>
 
 int hip = 0x00;
+float hipUpperBound = 100;
+float hipLowerBound = 10;
+
 int face = 0x01;
+float faceUpperBound = 150;
+float faceLowerBound = -150;
+
 int head = 0x02;
+float headUpperBound = 150;
+float headLowerBound = -150;
+
+long baudRate = 57600;
 
 void setup()
 {
   delay(2000);  //a delay to have time for serial monitor opening
-  Serial.begin(57600);    // Open serial communications
+  Serial.begin(baudRate);    // Open serial communications
   Serial.println("Begin");
-  Herkulex.begin(57600,10,11); //open serial with rx=10 and tx=11
+  Herkulex.begin(baudRate,10,11); //open serial with rx=10 and tx=11
   Herkulex.reboot(hip); //reboot first motor
   delay(500);
   Herkulex.initialize(); //initialize motors
   delay(2000);
+
+  //Change the baud rate of a single motor: 0x10=115200, 0x22=57600
+  //Herkulex.writeRegistryEEP(head, 0x04, 0x22);
+  //delay(2000);
 }
 
 void loop(){
+  potentiometerControl();
+}
+
+void potentiometerControl() {
   float hipRotation = ((float)analogRead(A0) / 3.2) - 160;
   float faceRotation = ((float)analogRead(A1) / 3.2) - 160;
   float headRotation = ((float)analogRead(A2) / 3.2) - 160;
 
-  rotateHipMotor(hipRotation, 0);
-  rotateFaceMotor(faceRotation, 0);
-  rotateHeadMotor(headRotation, 0);
+  rotateMotor(hip, hipRotation, 0, hipUpperBound, hipLowerBound);
+  rotateMotor(face, faceRotation, 0, faceUpperBound, faceLowerBound);
+  rotateMotor(head, headRotation, 0, headUpperBound, headLowerBound);
 
-  errorCheck();
+  errorCheck(hip);
+  errorCheck(face);
+  errorCheck(head);
   
   delay(33);
-}
-
-void rotateHipMotor(float position, int time) {
-  rotateMotor(hip, position, time, 100, 10);
-}
-
-void rotateFaceMotor(float position, int time) {
-  rotateMotor(face, position, time, 150, -150);
-}
-
-void rotateHeadMotor(float position, int time) {
-  rotateMotor(head, position, time, 150, -150);
 }
 
 void rotateMotor(int motorId, float position, int time, float upperBound, float lowerBound) {
@@ -53,19 +61,13 @@ void rotateMotor(int motorId, float position, int time, float upperBound, float 
   Herkulex.moveOneAngle(motorId, position, time, LED_BLUE);
 }
 
-void errorCheck() {
-  int hipStat = Herkulex.stat(hip);
-  int faceStat = Herkulex.stat(face);
-  int headStat = Herkulex.stat(head);
-
-  String hipError = "Hip: ";
-  hipError += hipStat;
-  String faceError = "Face: ";
-  faceError += faceStat;
-  String headError = "Head: ";
-  headError += headStat;
-  
-  Serial.println(hipStat);
-  Serial.println(faceStat);
-  Serial.println(headStat);
+void errorCheck(int motorId) {
+  int status = Herkulex.stat(motorId);
+  if (status != 0) {
+    String errorMessage = "Motor error on motor ID ";
+    errorMessage += motorId;
+    errorMessage += ": ";
+    errorMessage += status;
+    Serial.println(errorMessage);
+  }
 }
